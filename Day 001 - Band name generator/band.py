c4aab5050fbefa53b46cd340196af2da94483c50
Francis McCarthy <francis.john.mccarthy@googlemail.com>
2023-11-05 23:16:46 +0100

    Day 1

---
+++ Day 001 - Band name generator/band.py
@@ -0,0 +1,6 @@
+print("Hello")
+
+city_name = input("What city did you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print("Your band name is: " + city_name + " " + pet_name)
